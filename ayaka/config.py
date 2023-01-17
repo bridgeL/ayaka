eab4249d5b7074b013d47365a410cd16a21a502a
Su <wxlxy316@163.com>
2023-01-17 21:54:17 +0800

    更新版本号 0.0.0.5

--- ayaka/config.py
+++ ayaka/config.py
@@ -7,7 +7,7 @@ from .logger import logger, clogger
 from .model import AyakaSession
 from .helpers import ensure_dir_exists
 
-AYAKA_VERSION = "0.0.0.5b5"
+AYAKA_VERSION = "0.0.0.5"
 clogger.success(f"<y>ayaka</y> 当前版本 <y>{AYAKA_VERSION}</y>")
 
 data_path = ensure_dir_exists("data/ayaka")
